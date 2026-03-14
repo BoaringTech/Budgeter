b06fdd2b8e59d990a83168107d3a256f9b5ddbdb
thebearagenda <nathan.reyes.q@gmail.com>
2026-03-14 17:11:30 -0400

    Created AccountsController and SubcategoriesController.

---
+++ Budgeter.Server/Controllers/AccountsController.cs
@@ -0,0 +1,75 @@
+﻿using Budgeter.Server.DTOs;
+using Budgeter.Server.Entities;
+using Budgeter.Server.Repositories.Interfaces;
+using Budgeter.Server.Requests;
+using Budgeter.Server.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budgeter.Server.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountsController(IAccountService accountService, IAccountRepository accountRepository)
+        {
+            _accountService = accountService;
+            _accountRepository = accountRepository;
+        }
+
+        // GET /accounts/
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AccountDTO>>> GetAccounts()
+        {
+            IEnumerable<Account> users = await _accountRepository.GetAllAccountsAsync();
+            IEnumerable<AccountDTO> usersDTO = users.Select(_accountService.TranslateAccount);
+            return Ok(usersDTO);
+        }
+
+        // POST /accounts/
+        [HttpPost]
+        public async Task<ActionResult<AccountDTO>> CreateAccount([FromBody] CreateAccountRequest request)
+        {
+            try
+            {
+                Account user = await _accountRepository.CreateAccountAsync(request);
+                AccountDTO userDTO = _accountService.TranslateAccount(user);
+                return CreatedAtAction(nameof(CreateAccount), new { id = userDTO.Id }, userDTO);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                return StatusCode(500, "An error occurred while creating the transaction");
+            }
+        }
+
+        // PUT /accounts/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAccount(int id, UpdateAccountRequest request)
+        {
+            Account? user = await _accountRepository.UpdateAccountAsync(id, request);
+
+            if (user == null)
+                return NotFound();
+
+            AccountDTO userDTO = _accountService.TranslateAccount(user);
+
+            return Ok(userDTO);
+        }
+
+        // DELETE /accounts/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccount(int id)
+        {
+            bool deleted = await _accountRepository.DeleteAccountAsync(id);
+
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Budgeter.Server/Controllers/SubcategoriesController.cs
@@ -0,0 +1,75 @@
+﻿using Budgeter.Server.DTOs;
+using Budgeter.Server.Entities;
+using Budgeter.Server.Repositories.Interfaces;
+using Budgeter.Server.Requests;
+using Budgeter.Server.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budgeter.Server.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class SubcategoriesController : ControllerBase
+    {
+        private readonly ISubcategoryService _subcategoryService;
+        private readonly ISubcategoryRepository _subcategoryRepository;
+
+        public SubcategoriesController(ISubcategoryService subcategoryService, ISubcategoryRepository subcategoryRepository)
+        {
+            _subcategoryService = subcategoryService;
+            _subcategoryRepository = subcategoryRepository;
+        }
+
+        // GET /subcategories/
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SubcategoryDTO>>> GetSubcategorys()
+        {
+            IEnumerable<Subcategory> users = await _subcategoryRepository.GetAllSubcategoriesAsync();
+            IEnumerable<SubcategoryDTO> usersDTO = users.Select(_subcategoryService.TranslateSubcategory);
+            return Ok(usersDTO);
+        }
+
+        // POST /subcategories/
+        [HttpPost]
+        public async Task<ActionResult<SubcategoryDTO>> CreateSubcategory([FromBody] CreateSubcategoryRequest request)
+        {
+            try
+            {
+                Subcategory user = await _subcategoryRepository.CreateSubcategoryAsync(request);
+                SubcategoryDTO userDTO = _subcategoryService.TranslateSubcategory(user);
+                return CreatedAtAction(nameof(CreateSubcategory), new { id = userDTO.Id }, userDTO);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                return StatusCode(500, "An error occurred while creating the transaction");
+            }
+        }
+
+        // PUT /subcategories/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSubcategory(int id, UpdateSubcategoryRequest request)
+        {
+            Subcategory? user = await _subcategoryRepository.UpdateSubcategoryAsync(id, request);
+
+            if (user == null)
+                return NotFound();
+
+            SubcategoryDTO userDTO = _subcategoryService.TranslateSubcategory(user);
+
+            return Ok(userDTO);
+        }
+
+        // DELETE /subcategories/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSubcategory(int id)
+        {
+            bool deleted = await _subcategoryRepository.DeleteSubcategoryAsync(id);
+
+            if (!deleted)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
