df76cadb4b62d48d2644169cd3ca41508df07356
thebearagenda <nathan.reyes.q@gmail.com>
2026-03-19 00:51:27 -0400

    Should've been in the last commit. Setting Service, now included.

---
+++ Budgeter.Server/Services/Interfaces/ISettingService.cs
@@ -0,0 +1,10 @@
+﻿using Budgeter.Server.DTOs;
+using Budgeter.Server.Entities;
+
+namespace Budgeter.Server.Services.Interfaces
+{
+    public interface ISettingService
+    {
+        BooleanSettingDTO TranslateSetting(BooleanSetting s);
+    }
+}
---
+++ Budgeter.Server/Services/SettingService.cs
@@ -0,0 +1,17 @@
+﻿using Budgeter.Server.DTOs;
+using Budgeter.Server.Entities;
+using Budgeter.Server.Services.Interfaces;
+
+namespace Budgeter.Server.Services
+{
+    public class SettingService : ISettingService
+    {
+        public BooleanSettingDTO TranslateSetting(BooleanSetting s)
+        {
+            return new BooleanSettingDTO { 
+                Name = s.Name, 
+                Enabled = s.Enabled 
+            };
+        }
+    }
+}
